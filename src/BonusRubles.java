838bb7979a8fe733ddd93554087ced3310699531
Oleg Rytov <cattimofei@gmail.com>
2023-07-16 12:57:34 +0300

    add BonusRebles.java

---
+++ src/BonusRubles.java
@@ -0,0 +1,18 @@
+public class BonusRubles {
+    public static void main(String[] args) {
+        int initialAmount = 200; //начальный счет
+        int replenishment = 1100; // сумма пополнения
+
+        int bonus;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;}
+        else {
+            bonus = 0;}
+        int accountAmount = initialAmount + replenishment + bonus;
+
+        System.out.println("Сумма на счете = " + accountAmount); // Конечная сумма на счете
+
+
+        }
+
+    }
